b7c657a90a7653e25d30b895e9453835957a22e1
WindZQ <1049498972@qq.com>
2020-04-30 20:46:17 +0800

    Chapter 3 Part Problem Soluton

---
+++ Chapter_3_Sharing_Data_Between_Threads/3.6.cpp
@@ -0,0 +1,35 @@
+#include <mutex>
+
+class some_big_object
+{};
+
+void swap(some_big_object &lhs, some_big_object &rhs)
+{}
+
+class X
+{
+public:
+    X(some_big_object const &sd) : some_big_object(sd){}
+
+    friend void swap(X &lhs, X &rhs)
+    {
+        if(&lhs == &rhs)
+        {
+            return;
+        }
+
+        std::lock(lhs.m, rhs.m);
+        std::lock_guard<std::mutex> lock_a(lhs.m, std::adopt_lock);
+        std::lock_guard<std::mutex> lock_b(rhs.m, std::adopt_lock);
+        swap(lhs.some_detail, rhs.some_detail);
+    }
+
+private:
+    some_big_object some_detail;
+    mutex std::mutex m;
+};
+
+int main(void)
+{
+    return 0;
+}
---
+++ Chapter_3_Sharing_Data_Between_Threads/3.8.cpp
@@ -0,0 +1,65 @@
+#include <mutex>
+#include <stdexcept>
+
+class hierarchical_mutex
+{
+    std::mutex internal_mutex;
+    unsigned long const hierarchy_value;
+    unsigned long previous_hierarch_value;
+    static thread_local unsigned long this_thread_hierarchy_value;
+
+    void check_for_hierarchy_violation(void)
+    {
+        if(this_thread_hierarchy_value <= hierarchy_value)
+        {
+            throw std::logic_error("mutex hierarchy violated");
+        }
+    }
+
+    void update_hierarchy_value(void)
+    {
+        previous_hierarchy_value = this_thread_hierarchy_value;
+        this_thread_hierarchy_value = hierarchy_value;
+    }
+
+public:
+    explicit hierarchical_mutex(unsigned long value) :
+             hierarchy_value(value),
+             previous_hierarch_value(0)
+             {}
+
+    void lock(void)
+    {
+        check_for_hierarchy_violation();
+        internal_mutex.lock();
+        update_hierarchy_value();
+    }
+
+    void unlock(void)
+    {
+        this_thread_hierarchy_value = previous_hierarch_value;
+        internal_mutex.unlock();
+    }
+
+    bool try_lock(void)
+    {
+        check_for_hierarchy_violation();
+        if(!internal_mutex.try_lock())
+        {
+            return false;
+        }
+
+        update_hierarchy_value();
+        return true;
+    }
+};
+
+thread_local unsigned long hierarchical_mutex::this_thread_hierarcny_value(ULONG_MAX);
+
+int main(void)
+{
+    hierarchical_mutex m1(42);
+    hierarchical_mutex m2(2000);
+
+    return 0;
+}
