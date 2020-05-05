acc8b775b117b187d0d524764fd5b1a469edf9ed
WindZQ <1049498972@qq.com>
2020-05-05 21:06:58 +0800

    Chapter 4 Part Problem Solution

---
+++ Chapter_4_Synchronizing_Concurrent_Operations/4.9.cpp
@@ -0,0 +1,28 @@
+#include <deque>
+#include <mutex>
+#include <future>
+#include <thread>
+#include <utility>
+
+std::mutex m;
+std::deque<std::packaged_task<void()>> tasks;
+
+bool gui_shutdown_message_received();
+void get_and_process_gui_message();
+
+void gui_thread()
+{
+    while(!gui_shutdown_message_received())
+    {
+        get_and_process_gui_message();
+        std::packaged_task<void()> task;
+        {
+            std::lock_guard<std::mutex> lk(m);
+            if(tasks.empty())
+                continue;
+            task = std::move(tasks.front());
+            tasks.pop_front();
+        }
+        task();
+    }
+}
