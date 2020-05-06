b14c5be95aee7b742d51d41f3199d0671c14be1e
WindZQ <1049498972@qq.com>
2020-05-06 21:12:32 +0800

    Chapter 4 All Problem Solution

---
+++ Chapter_4_Synchronizing_Concurrent_Operations/4.15.cpp
@@ -0,0 +1,45 @@
+struct card_inserted
+{
+    std::string account;
+};
+
+class atm
+{
+    messaging::receiver incoming;
+    messaging::sender bank;
+    messaging::sender interface_hardware;
+    void (atm::*state)();
+    std::string account;
+    std::string pin;
+    void wait_for_card()
+    {
+        interface_hardware.send(display_enter_card());
+        incoming.wait();
+        .handle<card_inserted>
+             ([&](card_inserted const &msg)
+             {
+                 account = msg.account;
+                 pin = "";
+                 interface_hardware.send(display_enter_pin());
+                 state = &atm::getting_pin;
+             });
+    }
+    void getting_pin();
+
+public:
+    void run()
+    {
+        state = &atm::wait_for_card;
+        try
+        {
+            for(;;)
+            {
+                (this->*state)();
+            }
+        }
+        catch(messaging::close_queue const &)
+        {
+
+        }
+    }
+};
